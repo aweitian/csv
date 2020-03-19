06910e60d70de805baeddd0fef152fd47a5d8895
aw <aw@qq.com>
2020-03-19 18:59:22 +0800

    init

---
+++ test.php
@@ -0,0 +1,32 @@
+<?php
+require "csv.php";
+
+
+$csv = new Csv();
+$cv = array(
+  "ID",
+  "编号",
+  "姓名",
+  "cc",
+);
+
+
+
+$dl_data = array($cv);
+
+$dl_data[] = array(
+      111,
+      "30852695748569854",
+      "张三行",
+      "adf",
+  );
+$dl_data[] = array(
+      22,
+      "30852695748569852",
+      "大型",
+      "dsds",
+  );
+$date = date("Y-m-d");
+$csv->downloadCsvData($dl_data, "数据({$date}).csv");
+?>
+
